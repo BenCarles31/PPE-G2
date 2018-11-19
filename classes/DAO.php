0bd5e3572ce221902fe9fdfd6d692c83bf6eb647
Ben <benjamin.carles@limayrac.fr>
2018-11-19 14:34:30 +0100

    Update DAO.php

--- classes/DAO.php
+++ classes/DAO.php
@@ -1,35 +1,39 @@
 <?php
-class DAO {
+/**
+* Classe mère DAO
+*/
 
-    protected $pdo=null; // Objet de connexion
+abstract class DAO {
 
-    /**
-    * Méthode de connexion
-    * @throws Exception
-    */
+  protected $pdo = NULL;  // Objet de connexion
 
-    function __construct() {
-      // On récupère les paramètres de la base à partir des constantes de init.php
-      $user = 'root';
-      $password='';
-      $host='localhost';
-      $name='fredi';
-      // On construit le DSN
-      $dsn = 'mysql:host=' . $host . ';dbname=' . $name;
-      // Création de la connexion
-      try {
-        $pdo = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch (PDOException $e) {
-        echo("<p>Erreur lors de la connexion : " . $e->getMessage().'<p>');
-      }
-      $this->pdo = $pdo;
-    }
-
-    protected function executer($sql, $params = null) {
+  /**
+  * Ouvre une connexion à la base de données
+  * @throws PDOException
+  */
+  function get_connection() {
+    // On construit le DSN
+    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+    // On se connecte à la base de données
     try {
+      $this->pdo=new PDO($dsn, DB_USER, DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      throw new Exception("Erreur lors de la connexion à la base de données : " . $e->getMessage())."\nDSN : ".$dsn;
+    }
+  }
+
+  /**
+  * Exécute une requête SQL
+  * @param string $sql Requête SQL
+  * @param array $params Paramètres de la requête
+  * @return PDOStatement Résultats de la requête
+  */
+  protected function executer($sql, $params = null) {
+    try {
+      $this->get_connection();
       if ($params == null) {
-        $sth = $this->pdo->query($sql); // exécution directe
+        $sth = $this->pdo->query($sql);   // exécution directe
       } else {
         $sth = $this->pdo->prepare($sql); // requête préparée
         $sth->execute($params);
@@ -37,9 +41,7 @@ class DAO {
     } catch (PDOException $e) {
       throw new Exception("Erreur lors de la requête SQL : " . $e->getMessage()."\nSQL : ".$sql);
     }
-      return $sth; // Renvoie le handler du résultat de la requête SQL
-    }
-
+    return $sth;  // Retourne le PDOStatement
   }
 
-?>
+}  // class DAO
