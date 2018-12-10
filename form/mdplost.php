fde2e047bab1abbef1e48aed2dd8dae1020e4590
BenCarles31 <benjamin.carles@limayrac.fr>
2018-12-10 13:47:56 +0100

    Manuel et fichiers SQL

--- form/mdplost.php
+++ form/mdplost.php
@@ -1,11 +1,17 @@
 <?php
-$mail = isset($_POST['mail']) ? $_POST['mail'] : '';
+$mail = isset($_POST['mail']) ? $_POST['mail'] : '!!';
+$mail1 = isset($_POST['mail1']) ? $_POST['mail1'] : '??';
 $valider = isset($_POST['valider']) ? $_POST['valider'] : '0';
 $valid_modif = isset($_POST['valid_modif']) ? $_POST['valid_modif'] : '0';
+$mdp = isset($_POST['mdp']) ? $_POST['mdp'] : '???';
 
-$verif_bdd = $responsableDAO->findAdherentByEmail($mail);
-?>
+$verif_bdd = $responsableDAO->findAdherentByEmailPass($mail,$mdp);
+$mail2 = $verif_bdd->get_email();   //test
 
+echo ($mail2);  //affichage test
+
+  if($valider == 0  ){
+    ?>
 <form class="login-form bg-white p-6 mx-auto border bd-default win-shadow"
       action="#"
       method="POST">
@@ -17,48 +23,41 @@ $verif_bdd = $responsableDAO->findAdherentByEmail($mail);
       </div>
 </form>
 
-<?php 
+
+<?php
+
+}
+  if($mail==$verif_bdd->get_email()){
+  ?>
+  <form class="login-form bg-white p-6 mx-auto border bd-default win-shadow"
+    action="#"
+    method="POST">
+   <div class="form-group">
+        <input type="text" name="mdp" data-prepend="<span class='mif-envelop'>" placeholder="Saisir nouveau mdp" data-validate="required">
+        <input type="hidden" name="mail1" value="<?php $verif_bdd->get_email();?>">
+    </div>
+    <div class="form-group">
+    <button id="test_form" class="button success" name="valid_modif" value="1">Modification</button><br/>
+
+
+    <p>je suis la </p>
+
+    <?php echo ($verif_bdd->get_email());  //Test
+    $mail2 = $verif_bdd->get_email();?>
 
 
 
-  if($mail==$verif_bdd){
-    ?>
-    <form class="login-form bg-white p-6 mx-auto border bd-default win-shadow"
-      action="#"
-      method="POST">
-     <div class="form-group">
-          <input type="text" name="mdp" data-prepend="<span class='mif-envelop'>" placeholder="Saisir nouveau mdp" data-validate="required">
-      </div>
-      <div class="form-group">
-      <button id="test_form" class="button success" name="valid_modif" value="1">Modification</button><br/>
-      </div>
+    </div>
 </form>
 <?php
-    }
+}
 
-      if($valid_modif==1){
+   if($valid_modif != 0){
 
-          $responsableDAO->updatemdp($mdp);
+     echo($mail2);
+     echo ('lol');          //test condiditon
+     echo ($mdp);
+
+          $responsableDAO->updatemdp($mdp,$mail2);
          /* header('Location: index.php');*/
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+       }
